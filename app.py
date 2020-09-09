68e0a4556385af9ea9d4d9d74989116de943775a
Philip Zhang <jmslca123@gmail.com>
2020-09-09 14:26:42 -0700

    db config

--- app.py
+++ app.py
@@ -44,8 +44,7 @@ def create_app():
 
 
 app = create_app()
-with app.app_context():
-    db.create_all()
-
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True, host="0.0.0.0")
