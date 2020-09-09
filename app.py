3c41c3af50f3ed7cace2e5a634aa1c3b73d41b32
Philip Zhang <jmslca123@gmail.com>
2020-09-09 14:14:04 -0700

    heroku config

--- app.py
+++ app.py
@@ -43,8 +43,9 @@ def create_app():
     return app
 
 
+app = create_app()
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    app = create_app()
-    with app.app_context():
-        db.create_all()
     app.run(debug=True, host="0.0.0.0")
