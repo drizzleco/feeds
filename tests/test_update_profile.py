7e081d9ee229eb2f925722c9210f50d52e6c693d
Philip Zhang <jmslca123@gmail.com>
2020-09-10 12:26:20 -0700

    fix tests

--- tests/test_update_profile.py
+++ tests/test_update_profile.py
@@ -10,23 +10,26 @@ def update_profile(client, **kwargs):
 
 
 def test_update_fails_on_no_json(client):
-    resp = client.put("me")
+    register(client, "name", "username", "my@email.com", "password", "password")
+    resp = client.put("/me")
     assert resp.json.get("error") == "Missing JSON in request"
 
 
 def test_update_fails_on_invalid_email(client):
-    register(client, "name", "username", "email", "password", "password")
+    register(client, "name", "username", "my@email.com", "password", "password")
     resp = update_profile(client, email="email")
     assert resp.json.get("error") == "Invalid email"
 
 
 def test_update_nonmatching_passwords_fails(client):
-    register(client, "name", "username", "email", "password", "password")
+    register(client, "name", "username", "my@email.com", "password", "password")
     resp = update_profile(client, password="password", confirm="nomatch")
     assert resp.json.get("error") == "Passwords do not match"
 
 
 def test_update_succeeds(client):
     register(client, "name", "username", "my@email.com", "password", "password")
-    resp = update_profile(client, name="tester", username="bob", email="email@google.com")
+    resp = update_profile(
+        client, name="tester", username="bob", email="email@google.com"
+    )
     assert resp.json.get("message") == "Successfully updated!"
