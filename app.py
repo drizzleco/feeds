8c0ac6b6c3771449645494b2d8edff0a50db2e7b
Philip Zhang <code@philipzha.ng>
2024-02-02 12:07:35 -0800

    Production Deployment (#33)
    * heroku config
    
    * update reqs
    
    * db config
    
    * const to var
    
    * force https
    
    * use vue production version
    
    * hotfix
    
    * updates
    
    * supernetes deploy

--- app.py
+++ app.py
@@ -43,8 +43,8 @@ def create_app():
     return app
 
 
+app = create_app()
 if __name__ == "__main__":
-    app = create_app()
     with app.app_context():
         db.create_all()
     app.run(debug=True, host="0.0.0.0")
--- static/js/init.js
+++ static/js/init.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    if (!location.href.match('localhost') && location.href.match('http:'))
+        location.href = window.location.href.replace('http', 'https')
     setTimeout(function () {
         M.AutoInit();
     }, 150)
